a67b95c922053dfff8dbd0a14478760af533397e
darylgqy <darylgqy@gmail.com>
2018-07-24 17:11:02 +0800

    adding another view 12

--- music/views.py
+++ music/views.py
@@ -6,3 +6,9 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse('<h1> Music App Homepage</h1>')
+
+def detail(request, album_id):
+    return HttpResponse("<h2>details for album_id: " + str(album_id) + '</h2>')
+
+
+
