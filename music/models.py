6428e2e7600ee8a92e50310e5c794e24e8525222
darylgqy <darylgqy@gmail.com>
2018-07-24 17:46:16 +0800

    connect to db 13 (buggy)

--- music/models.py
+++ music/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Album(models.Model):
     artist = models.CharField(max_length=250)
@@ -10,11 +11,10 @@ class Album(models.Model):
     def __str__(self):
         return self.album_title + ' - ' + self.artist
 
+
 class Song(models.Model):
-    #Foreignkey links to an album(of unique ID), foreign is to link an element to a group(e.g album)
-    #on_delete = models.CASCADE means that if the album is deleted; the songs are deleted as well
+    # Foreignkey links to an album(of unique ID), foreign is to link an element to a group(e.g album)
+    # on_delete = models.CASCADE means that if the album is deleted; the songs are deleted as well
     album = models.ForeignKey(Album, on_delete=models.CASCADE)
     file_type = models.CharField(max_length=10)
     song_title = models.CharField(max_length=250)
-
-
--- music/views.py
+++ music/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+from .models import Album
 
 
 def index(request):
-    return HttpResponse('<h1> Music App Homepage</h1>')
+    # stores all albums in database into all_albums
+    # returns the html version of all albums
+    all_albums = Album.objects.all()
+    html = ''
+    for album in all_albums:
+        url = "/music/" + str(album.id) + "/"
+        html += '<a href="' + url + '">' + album.album_title + '</a><br>'
+    return HttpResponse(html)
+
 
 def detail(request, album_id):
-    return HttpResponse("<h2>details for album_id: " + str(album_id) + '</h2>')
-
-
-
+    return HttpResponse("<h2>details for album_id: " + str(album_id) +"</h2>")
