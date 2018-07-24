9833ca80e195e79fa80168d432b4677021fdfc54
darylgqy <darylgqy@gmail.com>
2018-07-24 15:51:51 +0800

    activating models 8

--- djangotest/settings.py
+++ djangotest/settings.py
@@ -29,7 +29,7 @@ ALLOWED_HOSTS = []
 
 
 # Application definition
-
+#when boot up -> djagngotest -> settings.py -> look at INSTALLED_APPS -> MODELS (in this case)
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'music.apps.MusicConfig',
 ]
 
 MIDDLEWARE = [
--- music/models.py
+++ music/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Album(models.Model):
+    artist = models.CharField(max_length=250)
+    album_title = models.CharField(max_length=500)
+    genre = models.CharField(max_length=100)
+    album_logo = models.CharField(max_length=1000)
+
+class Song(models.Model):
+    #Foreignkey links to an album(of unique ID), foreign is to link an element to a group(e.g album)
+    #on_delete = models.CASCADE means that if the album is deleted; the songs are deleted as well
+    album = models.ForeignKey(Album, on_delete=models.CASCADE)
+    file_type = models.CharField(max_length=10)
+    song_title = models.CharField(max_length=250)
+
+
