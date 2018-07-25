bc1d991095ef777a0c81760acb416a50a4d5ceb2
darylgqy <darylgqy@gmail.com>
2018-07-25 17:11:47 +0800

    favorite view function 24

--- music/views.py
+++ music/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 
 def index(request):
@@ -17,4 +17,19 @@ def detail(request, album_id):
     # except Album.DoesNotExist:
     #     raise Http404("Album does not exist")
     album = get_object_or_404(Album, pk=album_id)
-    return render(request, 'music/detail.html', {'album': album, })
+    return render(request, 'music/detail.html', {'album': album,})
+
+
+def favorite(request, album_id):
+    album = get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/detail.html', {
+            'album': album,
+            'error_message': "invalid song"
+        })
+    else:
+        selected_song.is_favorite = True
+        selected_song.save()
+        return render(request, 'music/detail.html', {'album': album,})
