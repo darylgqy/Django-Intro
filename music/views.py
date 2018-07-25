699ffbf28d91ad3e5a8a0cd09505e7de2a31445c
darylgqy <darylgqy@gmail.com>
2018-07-25 11:01:01 +0800

    raise http404 and linking detail page 16

--- music/views.py
+++ music/views.py
@@ -1,7 +1,4 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse
+from django.http import Http404
 from django.shortcuts import render
 from .models import Album
 
@@ -11,11 +8,14 @@ def index(request):
     all_albums = Album.objects.all()
     # django already set up to look into a template dir (if u create or it exists)
 
-    context = { 'all_albums': all_albums,}
-    #render shortcuts via integrating httpresponse into it
+    context = {'all_albums': all_albums, }
+    # render shortcuts via integrating httpresponse into it
     return render(request, 'music/index.html', context)
 
 
-
 def detail(request, album_id):
-    return HttpResponse("<h2>details for album_id: " + str(album_id) +"</h2>")
+    try:
+        album = Album.objects.get(pk=album_id)
+    except Album.DoesNotExist:
+        raise Http404("Album does not exist")
+    return render(request, 'music/detail.html', {'album': album, })
