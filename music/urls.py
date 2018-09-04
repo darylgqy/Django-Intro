4ecf6f097eaf4f260f23ba0a14a331aa8cdc530e
darylgqy <darylgqy@gmail.com>
2018-09-04 23:35:18 +0800

    Model Forms 30

--- music/urls.py
+++ music/urls.py
@@ -12,4 +12,7 @@ urlpatterns = [
     # /music/71/
     path("<int:pk>/", views.DetailView.as_view(), name='detail'),
 
+    # /music/album/add/
+    path("album/add/$", views.AlbumCreate.as_view(), name = 'album-add')
+
 ]
