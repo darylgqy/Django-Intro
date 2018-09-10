a1af174aeff334a86e3cc8e70f8a05d086c4b48c
darylgqy <darylgqy@gmail.com>
2018-09-10 09:50:22 +0800

    django 30,31,32

--- music/views.py
+++ music/views.py
@@ -1,9 +1,11 @@
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Album
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
+    context_object_name = 'all_albums'
 
     def get_queryset(self):
         return Album.objects.all()
@@ -15,3 +17,13 @@ class DetailView(generic.DetailView):
 class AlbumCreate(CreateView):
     model = Album
     fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+class AlbumUpdate(UpdateView):
+    model = Album
+    fields = ['artist', 'album_title', 'genre', 'album_logo']
+
+class AlbumDelete(DeleteView):
+    model = Album
+    #redirects to homepage
+    success_url = reverse_lazy('music:index')
+
