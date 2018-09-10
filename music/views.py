d264709a7c8126aeb2660b212bbb5211fec35ddf
darylgqy <darylgqy@gmail.com>
2018-09-10 10:19:00 +0800

    34 User Registration

--- music/views.py
+++ music/views.py
@@ -2,6 +2,10 @@ from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from django.urls import reverse_lazy
 from .models import Album
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.views.generic import View
+from .forms import UserForm
 
 class IndexView(generic.ListView):
     template_name = 'music/index.html'
